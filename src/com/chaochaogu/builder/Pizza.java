e9ed8bf227ba7dec90358857b4ccae7a6e3ec5ef
bash chaochao.gu <723454695@qq.com>
2019-08-10 21:11:40 +0800

    modify

--- src/com/chaochaogu/builder/Pizza.java
+++ src/com/chaochaogu/builder/Pizza.java
@@ -12,7 +12,6 @@ import java.util.Set;
  */
 public abstract class Pizza {
     public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
-
     final Set<Topping> toppings;
 
     abstract static class Builder<T extends Builder<T>> {
