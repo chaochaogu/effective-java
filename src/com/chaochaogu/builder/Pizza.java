ba0c00a1a60d74bc2df252039b49becc2072f779
gcc bash <723454695@qq.com>
2019-08-10 21:03:38 +0800

    modify Pizza

--- src/com/chaochaogu/builder/Pizza.java
+++ src/com/chaochaogu/builder/Pizza.java
@@ -1,12 +1,37 @@
 package com.chaochaogu.builder;
 
+import java.util.EnumSet;
+import java.util.Objects;
+import java.util.Set;
+
 /**
  * Builder pattern for class hierarchies
+ *
  * @author chaochao gu
  * @date 2019/8/10
  */
 public abstract class Pizza {
+    public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
 
+    final Set<Topping> toppings;
+
+    abstract static class Builder<T extends Builder<T>> {
+        EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
+
+        public T addTopping(Topping topping) {
+            toppings.add(Objects.requireNonNull(topping));
+            return self();
+        }
+
+        abstract Pizza build();
+
+        // Subclasses must override this method to return "this"
+        protected abstract T self();
+    }
+
+    Pizza(Builder<?> builder) {
+        toppings = builder.toppings.clone();
+    }
 
 
 }
