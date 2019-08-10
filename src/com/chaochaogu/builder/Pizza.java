3dc3e2b1bf1e2503d91f59410da95d46961bbecc
chaochao.gu <723454695@qq.com>
2019-08-10 22:47:24 +0800

    test2

--- src/com/chaochaogu/builder/Pizza.java
+++ src/com/chaochaogu/builder/Pizza.java
@@ -14,6 +14,7 @@ public abstract class Pizza {
     public enum Topping {HAM, MUSHROOM, ONION, PEPPER, SAUSAGE}
     final Set<Topping> toppings;
     int i = 1;
+    int b = 2;
     abstract static class Builder<T extends Builder<T>> {
         EnumSet<Topping> toppings = EnumSet.noneOf(Topping.class);
 
