a5760431c93b94a610ccd15fc7c6e34aa5691f0d
chaochao.gu <723454695@qq.com>
2019-08-23 17:24:56 +0800

    add

---
+++ src/com/chaochaogu/generictype/Stack.java
@@ -0,0 +1,42 @@
+package com.chaochaogu.generictype;
+
+import java.util.Arrays;
+import java.util.EmptyStackException;
+
+/**
+ * @author chaochao Gu
+ * @date 2019/8/23
+ */
+public class Stack<E> {
+
+    private E[] elements;
+
+    private int size;
+
+    private static final int DEFAULT_CAPACITY = 16;
+
+    @SuppressWarnings("unchecked")
+    public Stack() {
+        elements = (E[]) new Object[DEFAULT_CAPACITY];
+    }
+
+    public void push(E e) {
+        ensureCapacity();
+        elements[size++] = e;
+    }
+
+    public E pop() {
+        if (size == 0) {
+            throw new EmptyStackException();
+        }
+        E result = elements[--size];
+        elements[size] = null;
+        return result;
+    }
+
+    private void ensureCapacity() {
+        if (size == elements.length) {
+            elements = Arrays.copyOf(elements, 2 * size + 1);
+        }
+    }
+}
