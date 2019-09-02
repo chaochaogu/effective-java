8deaca96fb4d8763d0b96dc5bcde23406bf9cd34
chaochao.gu <723454695@qq.com>
2019-09-02 16:17:12 +0800

    add

---
+++ src/com/chaochaogu/override/Bigram.java
@@ -0,0 +1,40 @@
+package com.chaochaogu.override;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Can you spot the bug ?
+ *
+ * @author chaochao Gu
+ * @date 2019/9/2
+ */
+public class Bigram {
+
+    private final char first;
+
+    private final char second;
+
+    public Bigram(char first, char second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public boolean equals(Bigram bigram) {
+        return bigram.first == first && bigram.second == second;
+    }
+
+    public int hashCode() {
+        return 31 * first + second;
+    }
+
+    public static void main(String[] args) {
+        Set<Bigram> s = new HashSet<>();
+        for (int i = 0; i < 10; i++) {
+            for (char ch = 'a'; ch <= 'z'; ch++) {
+                s.add(new Bigram(ch, ch));
+            }
+        }
+        System.out.println(s.size());
+    }
+}
