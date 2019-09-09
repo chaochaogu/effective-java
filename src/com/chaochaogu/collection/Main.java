45fe6a1070e8ddf71909faad7fbec1c30d4060c2
chaochao.gu <723454695@qq.com>
2019-09-09 18:31:29 +0800

    add

--- src/com/chaochaogu/collection/Main.java
+++ src/com/chaochaogu/collection/Main.java
@@ -1,5 +1,7 @@
 package com.chaochaogu.collection;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
@@ -11,16 +13,20 @@ public class Main {
 
     public static void main(String[] args) {
 
-
+        Set<String> set = new HashSet<>();
+        set.add("1");
+        set.add("2");
+        set.add("3");
+        System.out.println(PowerSet.of(set));
     }
 
     // Adapter from Stream<E> to Iterable<E>
-    public static <E> Iterable<E> iterableOf(Stream<E> stream){
+    public static <E> Iterable<E> iterableOf(Stream<E> stream) {
         return stream::iterator;
     }
 
     // Adapter from Iterable<E> to Stream<E>
-    public static <E> Stream<E> streamOf(Iterable<E> iterable){
+    public static <E> Stream<E> streamOf(Iterable<E> iterable) {
         return StreamSupport.stream(iterable.spliterator(), false);
     }
 }
